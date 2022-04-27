f8affc6e5d81c228feeeaf21d1f01550f8b66966
moretti-matteo <matteomoretti2000@hotmail.it>
2022-04-27 15:04:27 +0200

    esercizio implementato

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const name = prompt("Enter your name:");
+const surname = prompt("Enter your surname:");
+const favoriteColor = prompt("Enter your favorite color:");
+
+//generates a random number from 0 to 99
+const randomNumber = Math.floor(Math.random() * 100);
+
+
+document.getElementById('container').classList.remove("hidden");
+document.getElementById('pwd').innerHTML = name + surname + favoriteColor + randomNumber;
+
+console.log(name + surname + favoriteColor + randomNumber);
