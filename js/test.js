5a9a92fa54e8327e5a022df187aa8dad95790cc3
moretti-matteo <matteomoretti2000@hotmail.it>
2022-04-27 17:33:19 +0200

    update test

--- js/test.js
+++ js/test.js
@@ -1,14 +1,15 @@
 function generatePwd() {
     const name = document.module.name.value;
     const surname = document.module.surname.value;
-    const favoriteColor = document.module.favoriteColor.value;
+    // const favoriteColor = document.module.favoriteColor.value;
     const randomNumber = Math.floor(Math.random() * 100);
+    const favoriteColor = document.module.colorList.value;
 
     if (!(name && surname && favoriteColor)) {
-        alert("uno o più campi non sono compilati!");
+        alert("One or more fields are not filled in");
         return 0;
     }else{
-        alert("Password generata: " + name + surname + favoriteColor + randomNumber);
+        alert("Password generated: " + name + surname + favoriteColor + randomNumber);
         return 1;
     }
 
